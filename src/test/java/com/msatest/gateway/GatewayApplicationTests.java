7e833cb183e82ebd382e9638354c86e9f135b219
Ju-Yeongmin <drodudals1@gmail.com>
2024-04-15 19:36:19 +0900

    first commit

---
+++ src/test/java/com/msatest/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msatest.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
